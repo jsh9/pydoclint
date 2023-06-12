31e6a0bd2bef1208f5933351d869dc6311a42ff7
jsh9 <25124332+jsh9@users.noreply.github.com>
2023-06-12 02:06:30 -0700

    Fix bug in type hint checking (#29)

---
+++ tests/data/google/star_args/cases2.py
@@ -0,0 +1,13 @@
+from typing import Dict, Tuple
+
+
+class A:
+    def __init__(self, *args: Tuple, **kwargs: Dict) -> None:
+        """
+        Do something.
+
+        Args:
+            *args: Args
+            **kwargs: Keyword args
+        """
+        pass
---
+++ tests/data/numpy/star_args/cases2.py
@@ -0,0 +1,16 @@
+from typing import Dict, Tuple
+
+
+class A:
+    def __init__(self, *args: Tuple, **kwargs: Dict) -> None:
+        """
+        Do something.
+
+        Parameters
+        ----------
+        *args :
+            Args
+        **kwargs :
+            Keyword args
+        """
+        pass
