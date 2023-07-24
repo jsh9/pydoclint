fc430fdabcc3032e1c5ad218d67ee7a916679def
real-yfprojects <real-yfprojects@users.noreply.github.com>
2023-07-24 08:21:42 +0200

    Make `docs/index.md` a symlink.
    * docs/index.md : Symlink to `README.md`
    
    * .pre_commit_helper_scripts/copy_readme.py : Delete file.
    
    * .pre-commit-config.yaml : Remove hook calling above script.

--- .pre_commit_helper_scripts/copy_readme.py
+++
@@ -1,7 +0,0 @@
-"""Copy `README.md` into `docs/index.md` at every pre-commit runs"""
-
-with open('README.md', encoding='UTF-8') as fp:
-    lines = fp.readlines()
-
-with open('docs/index.md', 'w', encoding='UTF-8') as fp:
-    fp.writelines(lines)
