2a62ac8e311addbd7f0e749799da6824deac4536
jsh9 <25124332+jsh9@users.noreply.github.com>
2025-10-25 22:46:40 -0400

    fix ast_types.py (all tests passed)

--- pydoclint/utils/ast_types.py
+++ pydoclint/utils/ast_types.py
@@ -2,12 +2,9 @@ from __future__ import annotations
 
 import ast
 import sys
-from typing import Union
 
-# typing.Union is still needed when defining custom types in Python 3.9.
-# It can be changed to `xxx | yyy` after Python 3.9 is dropped.
-FuncOrAsyncFuncDef = Union[ast.AsyncFunctionDef, ast.FunctionDef]
-ClassOrFunctionDef = Union[ast.ClassDef, ast.AsyncFunctionDef, ast.FunctionDef]
+FuncOrAsyncFuncDef = ast.AsyncFunctionDef | ast.FunctionDef
+ClassOrFunctionDef = ast.ClassDef | ast.AsyncFunctionDef | ast.FunctionDef
 
 LegacyBlockTypes = [
     ast.If,
@@ -20,9 +17,7 @@ LegacyBlockTypes = [
     ast.ExceptHandler,
 ]
 
-if sys.version_info < (3, 10):
-    BlockType = tuple(LegacyBlockTypes)
-elif sys.version_info < (3, 11):
-    BlockType = tuple(LegacyBlockTypes + [ast.match_case])
-else:
-    BlockType = tuple(LegacyBlockTypes + [ast.match_case, ast.TryStar])
+if sys.version_info >= (3, 11):
+    BlockType = (*LegacyBlockTypes, ast.match_case, ast.TryStar)
+else:  # Python 3.10 only, because this project doesn't support Python 3.9
+    BlockType = (*LegacyBlockTypes, ast.match_case)
